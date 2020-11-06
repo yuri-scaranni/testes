df73b6ee0ff478cd2fdd2884a3c1e546278d245b
yuri <yuri_scaranni@hotmail.com>
2020-11-05 21:46:26 -0300

    'fix'

--- sftp.py
+++ sftp.py
@@ -3,15 +3,15 @@ import sys
 
 myHostname = "ec2-18-229-162-55.sa-east-1.compute.amazonaws.com"
 myUsername = "ec2-user"
-myPassword = r"C:\Users\scaranni\Documents\AWS\scaranni_key.pem"
+myPassword = "/home/ec2-user/work_space/testes/scaranni_key.pem"
 
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
 
-file_path = '/home/ec2-user/work_space/texto.txt'
+file_path = '/home/ec2-user/work_space/testes/texto.txt'
 remote_dir = '/home/ec2-user/downloads'
 
-#python3 desafio01.py '/home/ec2-user/work_space/texto.txt' '/home/ec2-user/downloads'
+#python3 sftp.py '/home/ec2-user/work_space/texto.txt' '/home/ec2-user/downloads'
 
 with pysftp.Connection(host=myHostname, username=myUsername, private_key=myPassword, cnopts=cnopts) as sftp:
     print("Connection succesfully stablished ... ")
